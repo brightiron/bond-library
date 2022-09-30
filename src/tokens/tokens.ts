1f654881050ea7061b45dc33c5e6be2e1d24ac44
brightiron <brightiron@protonmail.com>
2022-09-30 07:34:46 -0400

    revert formatting

--- src/tokens/tokens.ts
+++ src/tokens/tokens.ts
@@ -36,7 +36,7 @@ export function getToken(address: string): Token | null {
 
 export const getUniqueApiIds = function(): {
   coingecko: Set<string>,
-} {
+  } {
   const coingecko: Set<string> = new Set();
 
   for (const token of TOKENS.values()) {
@@ -60,155 +60,155 @@ const mapReducer = (arr: any, [keys, val]: any) => [
 
 // @ts-ignore
 export const TOKENS = new Map<string, Token>([
+  [
     [
-      [
-        "eth",
-        "mainnet_0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".toLowerCase(),
-        "goerli_0x8b7fb00abb67ba04ce894b9e2769fe24a8409a6a".toLowerCase(),
-        "goerli_0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6".toLowerCase()
-      ],
-      {
-        name: "Ethereum",
-        symbol: "ETH",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, { source: "coingecko", apiId: "ethereum" }],
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.ETHEREUM_MAINNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&chainId=1"],
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6&chainId=5"]
-        ])
-      }
+      "eth",
+      "mainnet_0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".toLowerCase(),
+      "goerli_0x8b7fb00abb67ba04ce894b9e2769fe24a8409a6a".toLowerCase(),
+      "goerli_0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6".toLowerCase()
     ],
+    {
+      name: "Ethereum",
+      symbol: "ETH",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, { source: "coingecko", apiId: "ethereum" }],
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.ETHEREUM_MAINNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&chainId=1"],
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6&chainId=5"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0x41e38e70a36150d08a8c97aec194321b5eb545a5".toLowerCase()
-      ],
-      {
-        name: "Mock DAI (2)",
-        symbol: "DAI2",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, {
-              source: "custom",
-              customPriceFunction: async () => "1.00"
-            }]
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x41e38e70a36150d08a8c97aec194321b5eb545a5&chainId=5"]
-        ])
-      }
+      "goerli_0x41e38e70a36150d08a8c97aec194321b5eb545a5".toLowerCase()
     ],
+    {
+      name: "Mock DAI (2)",
+      symbol: "DAI2",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, {
+          source: "custom",
+          customPriceFunction: async () => "1.00"
+        }]
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x41e38e70a36150d08a8c97aec194321b5eb545a5&chainId=5"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0x326C977E6efc84E512bB9C30f76E30c160eD06FB".toLowerCase()
-      ],
-      {
-        name: "Mock Chainlink",
-        symbol: "LINK",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, {
-              source: "coingecko",
-              apiId: "chainlink"
-            }]
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x326C977E6efc84E512bB9C30f76E30c160eD06FB&chainId=5"]
-        ])
-      }
+      "goerli_0x326C977E6efc84E512bB9C30f76E30c160eD06FB".toLowerCase()
     ],
+    {
+      name: "Mock Chainlink",
+      symbol: "LINK",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, {
+          source: "coingecko",
+          apiId: "chainlink"
+        }]
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x326C977E6efc84E512bB9C30f76E30c160eD06FB&chainId=5"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C".toLowerCase()
-      ],
-      {
-        name: "Mock USDC",
-        symbol: "USDC",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, {
-              source: "custom",
-              customPriceFunction: async () => "1.00"
-            }]
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
-        ])
-      }
+      "goerli_0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C".toLowerCase()
     ],
+    {
+      name: "Mock USDC",
+      symbol: "USDC",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, {
+          source: "custom",
+          customPriceFunction: async () => "1.00"
+        }]
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60".toLowerCase()
-      ],
-      {
-        name: "Mock DAI 3",
-        symbol: "DAI3",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, {
-              source: "custom",
-              customPriceFunction: async () => "1.00"
-            }]
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
-        ])
-      }
+      "goerli_0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60".toLowerCase()
     ],
+    {
+      name: "Mock DAI 3",
+      symbol: "DAI3",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, {
+          source: "custom",
+          customPriceFunction: async () => "1.00"
+        }]
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0x2899a03ffdab5c90badc5920b4f53b0884eb13cc".toLowerCase()
-      ],
-      {
-        name: "Mock DAI 4",
-        symbol: "DAI4",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, {
-              source: "custom",
-              customPriceFunction: async () => "1.00"
-            }]
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
-        ])
-      }
+      "goerli_0x2899a03ffdab5c90badc5920b4f53b0884eb13cc".toLowerCase()
     ],
+    {
+      name: "Mock DAI 4",
+      symbol: "DAI4",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, {
+          source: "custom",
+          customPriceFunction: async () => "1.00"
+        }]
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0x79C950C7446B234a6Ad53B908fBF342b01c4d446".toLowerCase()
-      ],
-      {
-        name: "Mock USDT",
-        symbol: "USDT",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, {
-              source: "custom",
-              customPriceFunction: async () => "1.00"
-            }]
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
-        ])
-      }
+      "goerli_0x79C950C7446B234a6Ad53B908fBF342b01c4d446".toLowerCase()
     ],
+    {
+      name: "Mock USDT",
+      symbol: "USDT",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, {
+          source: "custom",
+          customPriceFunction: async () => "1.00"
+        }]
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0x3587b2F7E0E2D6166d6C14230e7Fe160252B0ba4".toLowerCase()
-      ],
-      {
-        name: "Mock COMP",
-        symbol: "COMP",
-        priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
-          [0, {
-              source: "coingecko",
-              apiId: "compound-governance-token"
-            }]
-        ]),
-        purchaseLinks: new Map<CHAIN_ID, string>([
-          [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
-        ])
-      }
+      "goerli_0x3587b2F7E0E2D6166d6C14230e7Fe160252B0ba4".toLowerCase()
     ],
+    {
+      name: "Mock COMP",
+      symbol: "COMP",
+      priceSources: new Map<number, SupportedPriceSource | CustomPriceSource>([
+        [0, {
+          source: "coingecko",
+          apiId: "compound-governance-token"
+        }]
+      ]),
+      purchaseLinks: new Map<CHAIN_ID, string>([
+        [CHAIN_ID.GOERLI_TESTNET, "https://app.compound.finance/"]
+      ])
+    }
+  ],
+  [
     [
-      [
-        "goerli_0x77195bB23B8Dac9F05D16092C7290BB7d1F7F1d3".toLowerCase()
-      ],
+      "goerli_0x77195bB23B8Dac9F05D16092C7290BB7d1F7F1d3".toLowerCase()
+    ],
       {
         name: "COMP-USDT LP",
         symbol: "COMP-USDT LP",
@@ -241,4 +241,4 @@ export const TOKENS = new Map<string, Token>([
         ]),
       },
     ],
-  ].reduce(mapReducer, []));
+].reduce(mapReducer, []));
